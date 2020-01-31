befe3bd3e93e041a545d5a56821d1ce5ac38cf2d
lizbethblu04 <lizbethblu04@hotmail.com>
2020-01-31 08:58:00 -0700

    Velocidad del movimiento del triangulo

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -13,7 +13,12 @@
 
 using namespace std;
 
+//Declarar una ventana
+GLFWwindow* window;
+
 float posXTriangulo = 0.0f, posYTriangulo = 0.0f;
+double tiempoActual, tiempoAnterior;
+double velocidadTriangulo = 0.7;
 
 void teclado_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -22,11 +27,49 @@ void teclado_callback(GLFWwindow* window, int key, int scancode, int action, int
 	{
 		posXTriangulo += 0.01;
 	}
+	if ((action == GLFW_PRESS || action == GLFW_REPEAT) && key == GLFW_KEY_LEFT)
+	{
+		posXTriangulo -= 0.01;
+	}
+	if ((action == GLFW_PRESS || action == GLFW_REPEAT) && key == GLFW_KEY_UP)
+	{
+		posYTriangulo += 0.01;
+	}
+	if ((action == GLFW_PRESS || action == GLFW_REPEAT) && key == GLFW_KEY_DOWN)
+	{
+		posYTriangulo -= 0.01;
+	}
 }
 
 void actualizar()
 {
-	//posXTriangulo += 0.0001;
+	tiempoActual = glfwGetTime();
+	double tiempoDiferencial = tiempoActual - tiempoAnterior;
+	int estadoDerecha = glfwGetKey(window, GLFW_KEY_RIGHT);
+	if (estadoDerecha == GLFW_PRESS)
+	{
+		posXTriangulo += velocidadTriangulo * tiempoDiferencial;
+	}
+
+	int estadoIzquierda = glfwGetKey(window, GLFW_KEY_LEFT);
+	if (estadoIzquierda == GLFW_PRESS)
+	{
+		posXTriangulo -= velocidadTriangulo * tiempoDiferencial;
+	}
+
+	int estadoArriba = glfwGetKey(window, GLFW_KEY_UP);
+	if (estadoArriba == GLFW_PRESS)
+	{
+		posYTriangulo += velocidadTriangulo * tiempoDiferencial;
+	}
+
+	int estadoAbajo = glfwGetKey(window, GLFW_KEY_DOWN);
+	if (estadoAbajo == GLFW_PRESS)
+	{
+		posYTriangulo -= velocidadTriangulo * tiempoDiferencial;
+	}
+
+	tiempoAnterior = tiempoActual;
 }
 
 void dibujar()
@@ -48,10 +91,7 @@ void dibujar()
 }
 
 int main()
-{
-    //Declarar una ventana
-	GLFWwindow* window;
-
+{  
 	//Si no se pudo iniciar GLFW terminamos ejecucion
 	if (!glfwInit())
 	{
@@ -83,7 +123,10 @@ int main()
 	cout << "Version OpenGL: " << versionGL;
 
 	//Establecemos que con cada evento de teclado, se llama a la función teclado_callback
-	glfwSetKeyCallback(window, teclado_callback);
+	//glfwSetKeyCallback(window, teclado_callback);
+
+	tiempoActual = glfwGetTime();
+	tiempoAnterior = tiempoActual;
 
 	//Ciclo de dibujo (Draw loop)
 	while (!glfwWindowShouldClose(window))
